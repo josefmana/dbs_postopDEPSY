c3c5654608512bbf25d0b31058dde01235f0ef98
josefmana <josef.mana@protonmail.com>
2023-03-06 19:38:58 +0100

    a series of DAGs of increasing complexity

--- dbs_postopDEPSY_causal_models.R
+++ dbs_postopDEPSY_causal_models.R
@@ -20,20 +20,40 @@ sapply( c("mods", "figs", "tabs", "sess"), function(i) if( !dir.exists(i) ) dir.
 
 # ---- building directed acyclic graphs ----
 
-# prepare a list to be filled-in by a bunch of gradually more complex DAGs
-d <- list()
+# prepare a list with names for each DAG to be build
+nms <- list( m1 = "dag #1: base assumptions only",
+             m2 = "dag #2: bi-factor depression",
+             m3 = "dag #3: bi-factor depression & LEDD",
+             m4 = "dag #4: bi-factor depression & medication",
+             m5 = "dag #5: bi-factor depression & medication affected by connectivity",
+             m6 = "dag #6: bi-factor depression & medication affected by connectivity stratified by regions"
+             )
 
-# dag no.1: base assumptions only (the arrow)
-d$m1 <- dagitty( "dag { C -> D1.i <- ID -> D0.i <- It -> D1.i <- D0.i } " ) %>%
-  # add coordinates to create an arrow
-  `coordinates<-`( list( x = c(D0.i = 0, D1.i = 1, ID = 1, It = 1, C = 2),
-                         y = c(D0.i = 1, D1.i = 1, ID = 0, It = 2, C = 1) ) )
+# one-by-one build the DAGs
+d <- list(
+  m1 = dagitty( "dag { C -> D1.i <- ID -> D0.i <- It -> D1.i <- D0.i } " ),
+  m2 = dagitty( "dag { C -> D1.i <- ID -> D0.i <- It <- F -> D1.i <- D0.i <- F -> It -> D1.i } " ),
+  m3 = dagitty( "dag { C -> D1.i <- L1 <- ID -> D0.i -> D1.i <- ID -> L0 -> D0.i; L0 -> L1; D0.i <- It -> D1.i <- F -> D0.i; F -> It } " ),
+  m4 = dagitty( "dag { C -> D1.i <- L1 <- ID -> D0.i -> D1.i <- ID -> L0 -> D0.i; D0.i <- It -> D1.i <- F -> D0.i; D1.i <- A1 <-ID -> A0 -> D0.i; F -> It; L0 -> L1; A0 -> A1 } " ),
+  m5 = dagitty( "dag { C -> D1.i <- L1 <- ID -> D0.i -> D1.i <- ID -> L0 -> D0.i; D0.i <- It -> D1.i <- F -> D0.i; D1.i <- A1 <-ID -> A0 -> D0.i; F -> It; L0 -> L1; A0 -> A1; A1 <- C -> L1 } " ),
+  m6 = dagitty( "dag { R -> C -> D1.i <- L1 <- ID -> D0.i -> D1.i <- ID -> L0 -> D0.i; D0.i <- It -> D1.i <- F -> D0.i; D1.i <- A1 <-ID -> A0 -> D0.i; F -> It; L0 -> L1; A0 -> A1; A1 <- C -> L1 } " )
+)
 
-# dag no.2: bi-factor depression (the hammer)
-d$m2 <- dagitty( "dag { C -> D1.i <- ID -> D0.i <- It <- F -> D1.i <- D0.i <- F -> It -> D1.i } " ) %>%
-  # add coordinates to create an arrow
-  `coordinates<-`( list( x = c(D0.i = 0, D1.i = 2, ID = 1, It = 1, C = 3, `F` = 1),
-                         y = c(D0.i = 1, D1.i = 1, ID = 0, It = 2, C = 1, `F` = 3) ) )
+# add coordinates to all DAGs
+for( i in names(d) ) d[[i]] <- d[[i]] %>% `coordinates<-`(
+  # listing coordinates for the most expansive DAG (m4 and m5), will apply to the smaller ones as well
+  list( x = c(D0.i = 0, D1.i = 2, ID = 1, It = 1, C = 3, `F` = 1, L0 = 0, L1 = 2, A0 = 0, A1 = 2, R = 3),
+        y = c(D0.i = 2, D1.i = 2, ID = 1, It = 3, C = 2, `F` = 4, L0 = 0, L1 = 0, A0 = 5, A1 = 5, R = 0) )
+)
+
+# separately plot each of the DAGs
+f <- lapply( names(d), function(i) d[[i]] %>% ggdag() + theme_dag(base_size = 13) + labs( title = nms[[i]] ) + theme( plot.title = element_text(face = "bold") ) )
+
+# arrange the DAGs intro a nice single plot
+( f[[1]] | f[[2]] | f[[3]] ) / ( f[[4]] | f[[5]] ) / ( f[[6]] ) + plot_layout( heights = c(1,1.5,2) )
+
+# save it
+ggsave( "figs/fig_1_causal_representation.jpeg", width = 1.2 * 11.8, height = 1.2 * 13.1 )
 
 
 # ---- session info ----
